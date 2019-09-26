f625f88907715a32dfe30326ae9a8e1429257a3d
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-09-26 13:42:25 +0530

    Boiler plate

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+	return (
+		<Router>
+			<Fragment>
+				<Navbar />
+				<div className='container'>
+					<Switch></Switch>
+				</div>
+			</Fragment>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import spinner from './spinner.gif';
+const Spinner = () => {
+	return (
+		<div>
+			<img
+				src={spinner}
+				alt='Loading...'
+				style={{
+					width: '200px',
+					margin: '40px auto',
+					display: 'block'
+				}}
+			/>
+		</div>
+	);
+};
+
+export default Spinner;
