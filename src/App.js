205c9b6e1558b3f752b2895ddf4611c72c44b75c
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-09-28 18:19:55 +0530

    migrated to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import store from './store';
