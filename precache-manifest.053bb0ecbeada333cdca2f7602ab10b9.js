687a6ab90efd50539113277aa1c172bd0de16b3f
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-09-28 18:10:40 +0530

    Updates

---
+++ precache-manifest.053bb0ecbeada333cdca2f7602ab10b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f1ed1710ec38d659b4604630e6f0af",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "8da6b4ae35f0a827a9ff",
+    "url": "/Pokedex/static/css/main.674a9146.chunk.css"
+  },
+  {
+    "revision": "eaa4e57e05a91dec8b13",
+    "url": "/Pokedex/static/js/2.bc848366.chunk.js"
+  },
+  {
+    "revision": "8da6b4ae35f0a827a9ff",
+    "url": "/Pokedex/static/js/main.49e4effa.chunk.js"
+  },
+  {
+    "revision": "03de357d9cbbbf8984c9",
+    "url": "/Pokedex/static/js/runtime-main.7a5fd437.js"
+  }
+]);
