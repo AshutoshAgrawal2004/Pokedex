2e62e088ef2b3c303ef6bc07a55b44d9cb21c22d
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-11-02 11:17:50 +0530

    Prev and Next btn

--- src/components/pokemon/PokemonDetail.js
+++ src/components/pokemon/PokemonDetail.js
@@ -19,11 +19,19 @@ import Varieties from './PokemonDetail/Varieties';
 import EvolutionChain from './PokemonDetail/EvolutionChain';
 
 const PokemonDetail = ({ clearCurrent, getPokemonDetail, pokemons, match }) => {
+	const PokeId = Number(match.params.id);
 	useEffect(() => {
 		clearCurrent();
-		getPokemonDetail(match.params.id);
+		getPokemonDetail(PokeId);
 	}, [match]);
-
+	const nextPokemon = () => {
+		clearCurrent();
+		getPokemonDetail(match.params.id + 1);
+	};
+	const prevPokemon = () => {
+		clearCurrent();
+		getPokemonDetail(match.params.id - 1);
+	};
 	const { loading, current_pokemon, current_pokemon_species } = pokemons;
 	if (
 		current_pokemon === null ||
@@ -52,31 +60,53 @@ const PokemonDetail = ({ clearCurrent, getPokemonDetail, pokemons, match }) => {
 	const bio = { height, weight, capture_rate, gender_rate };
 	return (
 		<Fragment>
-			<div className='card align-items-center'>
-				<Varieties varieties={varieties} />
+			<div
+				id='carouselExampleControls'
+				className='carousel slide'
+				data-ride='carousel'
+			>
+				<div className='card align-items-center'>
+					<Varieties varieties={varieties} />
 
-				<img
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-					onError={e => {
-						e.target.onerror = null;
-						e.target.src =
-							'https://ih1.redbubble.net/image.731955024.9007/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg';
-					}}
-					alt={name}
-					className='card-img-top pokeimg'
-				/>
-				<div className='card-body'>
-					<h1 className='card-title text-center text-capitalize'>
-						{name}
-					</h1>
-					<p className='card-text'>
-						{flavor_text_entries[1].language.name === 'en'
-							? flavor_text_entries[1].flavor_text
-							: flavor_text_entries[2].flavor_text}
-					</p>
+					<img
+						src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+						onError={e => {
+							e.target.onerror = null;
+							e.target.src =
+								'https://ih1.redbubble.net/image.731955024.9007/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg';
+						}}
+						alt={name}
+						className='card-img-top pokeimg'
+					/>
+					<div className='card-body'>
+						<h1 className='card-title text-center text-capitalize'>
+							{name}
+						</h1>
+						<p className='card-text'>
+							{flavor_text_entries[1].language.name === 'en'
+								? flavor_text_entries[1].flavor_text
+								: flavor_text_entries[2].flavor_text}
+						</p>
+					</div>
 				</div>
+				<h5 className='text-center'>Evolution</h5>
+				<Link
+					className='carousel-control-prev text-danger'
+					to={`/pokemon/${PokeId - 1}`}
+					role='button'
+					data-slide='prev'
+				>
+					<i class='fas fa-chevron-left'></i>
+				</Link>
+				<Link
+					className='carousel-control-next text-danger'
+					to={`/pokemon/${PokeId + 1}`}
+					role='button'
+					data-slide='next'
+				>
+					<i class='fas fa-chevron-right'></i>
+				</Link>
 			</div>
-			<h5 className='text-center'>Evolution</h5>
 			<EvolutionChain evolutionURL={evolution_chain.url} />
 			<Abilities key={uuid.v4()} abilities={abilities} />
 			<Bio key={uuid.v4()} bio={bio} />
